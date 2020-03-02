e3e8d70a381d8f7beb34ee4ea354776caf13d73a
sven3270350 <sven3270350@gmail.com>
2020-03-02 11:40:42 +0600

    Add payment and shipping method crud

--- app/Services/Admin/PaymentMethodService.php
+++ app/Services/Admin/PaymentMethodService.php
@@ -4,7 +4,85 @@
 namespace App\Services\Admin;
 
 
+use App\PaymentMethod;
+use Exception;
+
 class PaymentMethodService
 {
+    /**
+     * @param string $name
+     * @return array
+     */
+    public function create (string $name) :array {
+        try {
+            PaymentMethod::create([
+                'name' => $name
+            ]);
+            return [
+                'success' => true,
+                'message' => 'Payment Method has been saved'
+            ];
+        } catch (Exception $e) {
+            return [
+                'success' => false,
+                'message' => 'Failed to create Payment Method'
+            ];
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function find (int $id) {
+        return PaymentMethod::find($id);
+    }
+
+    /**
+     * @param int $payment_method_id
+     * @param string $name
+     * @return array
+     */
+    public function update (int $payment_method_id , string $name) :array {
+        try {
+            $paymentMethod  =  PaymentMethod::where('id',$payment_method_id)->update([
+                'name' => $name
+            ]);
+            if (!$paymentMethod) {
+                return ['success' => false, 'message' => 'Payment Method not found'];
+            }
+            return [
+                'success' => false,
+                'message' => 'Payment Method has been updated'
+            ];
+        } catch (Exception $e) {
+            return [
+                'success' => true,
+                'message' => 'Something went wrong'
+            ];
+        }
+    }
+
+    /**
+     * @param int $payment_method_id
+     * @return array
+     */
+    public function delete (int $payment_method_id) :array {
+        try {
+            $paymentMethod  =  PaymentMethod::where('id',$payment_method_id)->delete();
+            if (!$paymentMethod) {
+                return ['success' => false, 'message' => 'Payment Method not found'];
+            }
+            return [
+                'success' => false,
+                'message' => 'Payment Method has been deleted'
+            ];
+        } catch (Exception $e) {
+            return [
+                'success' => true,
+                'message' => 'Something went wrong'
+            ];
+        }
+    }
 
 }
--- app/Services/Admin/ShippingMethodService.php
+++ app/Services/Admin/ShippingMethodService.php
@@ -4,7 +4,88 @@
 namespace App\Services\Admin;
 
 
+use App\ShippingMethod;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+
 class ShippingMethodService
 {
+    /**
+     * @param string $name
+     * @return array
+     */
+    public function create (string $name) :array {
+        try {
+            ShippingMethod::create([
+                'name' => $name,
+            ]);
+            return [
+                'success' => true,
+                'message' => __('Shipping Method has been created')
+            ];
+        } catch (Exception $e) {
+            return [
+                'success' => false,
+                'message' => __('Failed to create Shipping Method')
+            ];
+        }
+    }
 
+    /**
+     * @return ShippingMethod[]|Collection
+     */
+    public function shippingMethods () {
+        return ShippingMethod::all();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function find (int $id) {
+        return ShippingMethod::find($id);
+    }
+
+    /**
+     * @param int $shipping_method_id
+     * @param string $name
+     * @return array
+     */
+    public function update (int $shipping_method_id, string $name) :array {
+        try {
+            $shippingMethod = ShippingMethod::where('id', $shipping_method_id)->update([
+                'name' => $name,
+            ]);
+            if (!$shippingMethod) {
+                return ['success' => false, 'message' => __('Shipping Method not found')];
+            }
+
+            return [
+                'success' => true,
+                'message' => __('Shipping Method has been updated')
+            ];
+        } catch (Exception $e) {
+            return [
+                'success' => false,
+                'message' => __('Something went wrong')
+            ];
+        }
+    }
+
+    /**
+     * @param int $shipping_method_id
+     * @return array
+     */
+    public function delete (int $shipping_method_id) :array {
+        try {
+            $shippingMethod = ShippingMethod::where('id', $shipping_method_id)->delete();
+            if (!$shippingMethod) {
+                return ['success' => false, 'message' => __('Shipping Method not found')];
+            }
+
+            return ['success' => true, 'message' => __('Shipping Method has been deleted')];
+        } catch (Exception $e) {
+            return ['success' => false, 'message' => __('Something went wrong')];
+        }
+    }
 }
