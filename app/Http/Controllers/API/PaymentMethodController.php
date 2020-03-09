41e87cd8e46de481210596b68e35dc6e7f113e10
sven3270350 <sven3270350@gmail.com>
2020-03-09 20:34:01 +0600

    Added payment method controller

---
+++ app/Http/Controllers/API/PaymentMethodController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Admin\PaymentMethodService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Exception;
+
+class PaymentMethodController extends Controller
+{
+    //
+    protected $paymentMethodService;
+
+    /**
+     * AuthorController constructor.
+     */
+    public function __construct () {
+        $this->paymentMethodService = new PaymentMethodService();
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function create (Request $request) {
+        try {
+            //Validation required
+            $name = $request->name;
+            $createPaymentMethodResponse = $this->paymentMethodService->create($name);
+            return response()->json([
+                'success' => true,
+                'message' => $createPaymentMethodResponse['message']
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function update (Request $request) {
+        try {
+            //Validation required
+            $payment_method_id = $request->payment_method_id;
+            $name = $request->name;
+            $updatePaymentMethodResponse = $this->paymentMethodService->update($payment_method_id,$name);
+            return response()->json([
+                'success' => true,
+                'message' => $updatePaymentMethodResponse['message']
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function find ($id) {
+        $paymentMethod = $this->paymentMethodService->find($id);
+        return response()->json([
+            'success' => false,
+            'book' => $paymentMethod,
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function authors () {
+        $paymentMethods = $this->paymentMethodService->paymentMethods();
+        return response()->json([
+            'success' => false,
+            'book' => $paymentMethods,
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function delete ($id) {
+        try {
+            $deletePaymentMethodResponse = $this->paymentMethodService->delete($id);
+            return response()->json([
+                'success' => true,
+                'message' => $deletePaymentMethodResponse['message']
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => true,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+}
