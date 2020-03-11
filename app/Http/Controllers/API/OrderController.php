1ea6bc870634a73565b7d2b0aa0ade668a1b3d89
sven3270350 <sven3270350@gmail.com>
2020-03-11 20:29:59 +0600

    Added order controller

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\OrderService;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+    protected $orderService;
+
+    /**
+     * OrderController constructor
+     */
+    public function __construct() {
+        $this->orderService = new OrderService();
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function placeOrder (Request $request) {
+        try {
+            $user = Auth::user();
+            $shippingData = $request->shippingData;
+            $orderData = $request->orderData;
+            $placeOrderResponse = $this->orderService->place($user->id,$shippingData,$orderData);
+            return response()->json(['success' => true, 'message' => $placeOrderResponse['message']]);
+        } catch (Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()]);
+        }
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function getUserOrders () {
+        $user = Auth::user();
+        $userOrders = $this->orderService->get($user->id);
+        return response()->json([
+            'success' => true,
+            'message' => $userOrders['message'],
+            'orders' => $userOrders['orders']
+        ]);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function orders () {
+        $allOrders  = $this->orderService->orders();
+        return response()->json([
+            'success' => true,
+            'message' => $allOrders['message'],
+            'orders' => $allOrders['orders']
+        ]);
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show (int $id) {
+        $order = $this->orderService->find($id);
+        return response()->json([
+            'success' => true,
+            'message' => $order['message'],
+            'order' => $order['order']
+        ]);
+    }
+
 }
